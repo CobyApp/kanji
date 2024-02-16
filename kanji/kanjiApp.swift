dc92bcf4a43312ce66707c6c021db65076a6c0e4
Coby <coby5502@gmail.com>
2024-02-17 00:29:54 +0900

    Initial Commit

---
+++ kanji/kanjiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanjiApp.swift
+//  kanji
+//
+//  Created by Coby on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct kanjiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
